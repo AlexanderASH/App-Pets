db442ced2b635b7c050c49aff0a2364c0e1025aa
Alexander Sanchez <pcua2014@hotmail.com>
2021-09-12 09:44:44 -0400

    adding copy of constants

---
+++ lib/src/commons/utils/constants.copy.dart
@@ -0,0 +1,6 @@
+// Change for your data and rename file for only constants
+const String HOST = "YOUR IP LOCAL";
+const int PORT = 5000;// Port used in backend
+const String PROTOCOL = "http";
+
+const String PETS = "api/v1/pets";
--- lib/src/commons/utils/constants.dart
+++
@@ -1,6 +0,0 @@
-// Change for your data
-const String HOST = "192.168.100.4";
-const int PORT = 5000;
-const String PROTOCOL = "http";
-
-const String PETS = "api/v1/pets";
